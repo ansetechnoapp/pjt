3702c7a95d81a12d78653c1073f0f517b2204c27
ansetechnoapp <129697695+ansetechnoapp@users.noreply.github.com>
2024-06-12 18:29:26 +0100

    edit file process-html mjs

--- process-html.mjs
+++ process-html.mjs
@@ -3,7 +3,8 @@ import { globby } from 'globby'
 import { minify } from 'html-minifier'
 
 // Get all HTML files from the output directory
-const path = './.vercel/output/static'
+const path = 'dist'
+// const path = './.vercel/output/static'
 const files = await globby(`${path}/**/*.html`)
 
 await Promise.all(
